c5fceca6ed7259b26d0f0329572022ac3d62ba17
Palash A <palash.agarwal.96@gmail.com>
2021-10-06 18:45:18 -0400

    skel 6/

---
+++ src/web3/contracts/faucet.sol
@@ -0,0 +1,10 @@
+/*
+ * Community fund held at tampopo.eth
+ * Deposit any token, is converted to ETH
+ * Faucet used to create personas
+ *  ie, <persona>.tampopo.eth
+ *  and, mint 69 TP in their name
+ * also used to publish stories 
+ *  custom RLN to prevent abuse?
+ * 
+ */
---
+++ src/web3/contracts/persona.sol
@@ -0,0 +1,7 @@
+
+/* 
+ * Create a new persona at tampopo.eth
+ * Requires custom seed
+ * Mints 69 TP to <persona>.tampopo.eth
+ *
+ */
