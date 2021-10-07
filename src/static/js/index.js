e1780353dd7158b9d0a432e7390fd9b748e31924
Palash A <palash.agarwal.96@gmail.com>
2021-10-07 17:51:47 -0400

    skel 6.3/

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -1,1 +1,16 @@
-/* First script to be loaded */
+/* 
+ * First script to be loaded, does essential website work
+ * 
+ * - fetches and appends components
+ * - checks for personas stored locally
+ * - sets up trotter links and syncs trees
+ * - 
+ * 
+ */
+
+
+function metatags() {
+  // Append `meta` tags to `head` for social network cards
+  // `itemprop` and `property` if in body (ie, popup)
+
+}
